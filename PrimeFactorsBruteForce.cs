c28233568d1c5f1d540214d65524d9325ebf949a
LokeshKiran <lokeshkskiran@gmail.com>
2022-08-29 18:09:36 +0530

    [Lokesh]Added:prime factors identification problem

---
+++ PrimeFactorsBruteForce.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograme
+{
+    internal class PrimeFactorsBruteForce
+    {
+        public static void PrimeFactors()
+        {
+            Console.WriteLine("Enter the number :");
+            int number = int.Parse(Console.ReadLine());
+            for(int i = 2; i*i <= number; i++)
+            {
+                while (number % i == 0)
+                {
+                    number = number / i;
+                    Console.WriteLine(i);
+                }
+               
+
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace BasicCorePrograme
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Basic Core Programes\n");
-            Console.WriteLine("1.Flip Coin Problem\n2.Leap Year Identification\n3.PowerOfTwoComputation\n4.Harmonic Number");
+            Console.WriteLine("1.Flip Coin Problem\n2.Leap Year Identification\n3.PowerOfTwoComputation\n4.Harmonic Number\n5.PrimeFactors of a given number");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -26,6 +26,9 @@ namespace BasicCorePrograme
                     break;
                 case 4:
                     HarmonicNumber.HarmonicNumberCom();
+                    break;
+                case 5:
+                    PrimeFactorsBruteForce.PrimeFactors();
                     break;
 
                 default:
