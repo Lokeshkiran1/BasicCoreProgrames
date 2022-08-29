7d0e60c79821ede47bed9dd0e4d62be7df5fb079
LokeshKiran <lokeshkskiran@gmail.com>
2022-08-29 20:40:02 +0530

    [Lokesh]Added:Greatest of three number identification problem

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace BasicCorePrograme
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Basic Core Programes\n");
-            Console.WriteLine("1.Flip Coin Problem\n2.Leap Year Identification\n3.PowerOfTwoComputation\n4.Harmonic Number\n5.PrimeFactors of a given number\n6.Compute Quotient and remainder\n7.Even or odd number\n8.Swapping of two numbers\n9.Vowel and Consonant ");
+            Console.WriteLine("1.Flip Coin Problem\n2.Leap Year Identification\n3.PowerOfTwoComputation\n4.Harmonic Number\n5.PrimeFactors of a given number\n6.Compute Quotient and remainder\n7.Even or odd number\n8.Swapping of two numbers\n9.Vowel and Consonant\n10.Largest of three numbers ");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -42,6 +42,9 @@ namespace BasicCorePrograme
                     break;
                 case 9:
                     VowelsConsonant.VowelsConsonantIdentification();
+                    break;
+                case 10:
+                    LargestOfThreeNumbers.largest();
                     break;
 
                 default:
